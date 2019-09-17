a45223222f977a1b295ddf5795a975d044c08919
First <suphanan.yolao@gmail.com>
2019-09-17 18:01:13 +0700

    mount position

--- src/views/LocalStorageLayout.jsx
+++ src/views/LocalStorageLayout.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import RGL, { WidthProvider} from "react-grid-layout";
 import { Card, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
+import _ from "lodash";
 import '../App.css';
+import firebase from "firebase/app"
+import "firebase/firestore"
 
 const ReactGridLayout = WidthProvider(RGL);
 const originalLayout = getFromLS("layout") || [];
@@ -21,6 +24,7 @@ class LocalStorageLayout extends React.PureComponent {
 
     this.state = {
       layout: JSON.parse(JSON.stringify(originalLayout))
+      // layout: []
     };
 
     this.onLayoutChange = this.onLayoutChange.bind(this);
@@ -35,9 +39,64 @@ class LocalStorageLayout extends React.PureComponent {
 
   onLayoutChange(layout) {
     /*eslint no-console: 0*/
-    saveToLS("layout", layout);
-    this.setState({ layout });
-    this.props.onLayoutChange(layout); // updates status display
+
+    // const firestore = firebase.firestore();
+    // const washingtonRef = firestore.collection("layout").doc("DC");
+    // // Set the "capital" field of the city 'DC'
+    // return washingtonRef.update({
+    //     capital: true
+    // })
+    // .then(function() {
+    //     console.log("Document successfully updated!");
+    // })
+    // .catch(function(error) {
+    //     // The document probably doesn't exist.
+    //     console.error("Error updating document: ", error);
+    // });
+    console.log('layout  ', layout)
+    // saveToLS("layout", layout);
+    // this.setState({ layout });
+    // this.props.onLayoutChange(layout); // updates status display
+    console.log('move  ', this.state.layout)
+
+  }
+
+  componentDidMount() {
+    let firestore = firebase.firestore();
+    firestore.collection("layout").get().then((snapshot) => {
+      const listCollect = snapshot.docs.map(v => {
+        return {
+          cid: v.id,
+          ...v.data()
+        }
+      })
+      // console.log('Layout ', this.state.layout)
+      this.setState({
+        layout: listCollect
+      })
+      console.log('mount ', listCollect)
+
+  });
+  }
+
+  createElement(el) {
+    // const i = el.cid;
+
+    console.log('Create ', el)
+    return (
+      <div key={el.i} data-grid={el} style={{width: '100%', height: '100%'}}>
+        <div className="content">
+
+          <Card style={{width: '100%', height: '100%', position: 'absolute',top: 0,bottom: 0}}>
+            <CardBody>
+                <CardTitle>Card title {el.cid} </CardTitle>
+                <CardSubtitle className="mb-2 text-muted">Card subtitle</CardSubtitle>
+                <CardText><span >1</span></CardText>
+            </CardBody>
+          </Card>
+        </div>
+      </div>
+    );
   }
 
   render() {
@@ -47,53 +106,8 @@ class LocalStorageLayout extends React.PureComponent {
         <ReactGridLayout
           {...this.props}
           layout={this.state.layout}
-          onLayoutChange={this.onLayoutChange}
-        >
-          <div key="1" data-grid={{ w: 2, h: 3, x: 0, y: 0 }}>
-            <Card style={{width: '100%', height: '100%'}}>
-              <CardBody>
-                  <CardTitle>Card title CCC </CardTitle>
-                  <CardSubtitle className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                  <CardText><span className="text">1</span></CardText>
-              </CardBody>
-            </Card>
-          </div>
-          <div key="2" data-grid={{ w: 2, h: 3, x: 2, y: 0 }}>
-          <Card style={{width: '100%', height: '100%'}}>
-              <CardBody>
-                  <CardTitle>Card title CCC </CardTitle>
-                  <CardSubtitle className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                  <CardText><span className="text">1</span></CardText>
-              </CardBody>
-            </Card>
-          </div>
-          <div key="3" data-grid={{ w: 2, h: 3, x: 4, y: 0 }}>
-          <Card style={{width: '100%', height: '100%'}}>
-              <CardBody>
-                  <CardTitle>Card title CCC </CardTitle>
-                  <CardSubtitle className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                  <CardText><span className="text">1</span></CardText>
-              </CardBody>
-            </Card>
-          </div>
-          <div key="4" data-grid={{ w: 2, h: 3, x: 6, y: 0 }}>
-          <Card style={{width: '100%', height: '100%'}}>
-              <CardBody>
-                  <CardTitle>Card title CCC </CardTitle>
-                  <CardSubtitle className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                  <CardText><span className="text">1</span></CardText>
-              </CardBody>
-            </Card>
-          </div>
-          <div key="5" data-grid={{ w: 2, h: 3, x: 8, y: 0 }}>
-          <Card style={{width: '100%', height: '100%'}}>
-              <CardBody>
-                  <CardTitle>Card title CCC </CardTitle>
-                  <CardSubtitle className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                  <CardText><span className="text">1</span></CardText>
-              </CardBody>
-            </Card>
-          </div>
+          onLayoutChange={this.onLayoutChange} >
+          {_.map(this.state.layout, el => this.createElement(el))}
         </ReactGridLayout>
       </div>
 
